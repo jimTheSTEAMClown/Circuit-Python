5270713dc252fbcd9b746bc37a82e6cb5d828d3f
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-01-09 18:43:38 -0800

    Create CPX-Blink.pl

---
+++ CPX-Blink.pl
@@ -0,0 +1,14 @@
+# Save on CPX/CPB as code.py
+# Blinks the top right RED LED
+import board
+import digitalio
+import time
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(0.1)
+    led.value = False
+    time.sleep(0.1)
