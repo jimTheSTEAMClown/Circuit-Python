485b75a4af9238c8f1bf03b84b28bf94ea79ed41
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-02-25 13:45:43 -0800

    Update and rename CPX-NioPixel-Blink.pl to CPX-NeoPixel-Blink.pl

--- CPX-NeoPixel-Blink.pl
+++ CPX-NeoPixel-Blink.pl
@@ -17,6 +17,9 @@ import time
 import board
 import neopixel
 
+# Calls the circuit Python neopixel library, specifies the default board 
+# pins for the Neopixels, and the number of neopixels to access.  Returns a 
+# list 'pixels' of indexable neopixles
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
 while True:
