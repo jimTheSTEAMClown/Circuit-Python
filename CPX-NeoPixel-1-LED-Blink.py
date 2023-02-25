1a46f555141044b63ba829439fefc25db77fc257
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-02-25 13:48:36 -0800

    Update and rename CPX-NioPixel-1-LED-Blink.pl to CPX-NeoPixel-1-LED-Blink.py

--- CPX-NeoPixel-1-LED-Blink.py
+++ CPX-NeoPixel-1-LED-Blink.py
@@ -17,9 +17,14 @@ import time
 import board
 import neopixel
 
+# Calls the circuit Python neopixel library, specifies the default board 
+# pins for the Neopixels, and the number of neopixels to access.  Returns a 
+# list 'pixels' of indexable neopixles
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
 while True:
+    # indexes the first element of the 'pixels' list and points to the 
+    # first (and only) Neopixel in the 'pixels' list
     pixels[0]=(255, 0, 0)
     time.sleep(0.5)
     pixels.fill((0, 0, 0))
