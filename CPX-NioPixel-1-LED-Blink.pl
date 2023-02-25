2eafbe9924c20ebeb8af53cd40a3ee26baabf584
Jim Burnham <jim.the.steam.clown@gmail.com>
2023-02-25 13:37:52 -0800

    Create CPX-NioPixel-1-LED-Blink.pl

---
+++ CPX-NioPixel-1-LED-Blink.pl
@@ -0,0 +1,26 @@
+# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Blink example for boards with ONLY a NeoPixel LED (e.g. without a built-in red LED).
+Includes QT Py and various Trinkeys.
+Requires two libraries from the Adafruit CircuitPython Library Bundle.
+Download the bundle from circuitpython.org/libraries and copy the
+following files to your CIRCUITPY/lib folder:
+* neopixel.mpy
+* adafruit_pixelbuf.mpy
+Once the libraries are copied, save this file as code.py to your CIRCUITPY
+drive to run it.
+"""
+import time
+import board
+import neopixel
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+while True:
+    pixels[0]=(255, 0, 0)
+    time.sleep(0.5)
+    pixels.fill((0, 0, 0))
+    time.sleep(0.5)
